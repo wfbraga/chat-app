4f5d8c46b640d59dca9193647fde518bc6b0cbd6
Wagner Braga <wfbraga@hotmail.com>
2026-03-19 15:28:58 -0300

    just identationss

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :assigned_conversations, foreign_key: :staff_id, class_name: 'Conversation'
   has_many :sent_messages, foreign_key: :sender_id, class_name: 'Message'
 
-  enum role: { user: 0, admin: 1, support: 2, sales: 3}
+  enum role: { user: 0, admin: 1, support: 2, sales: 3 }
 
   def staff?
     admin? || support? || sales?
